9565e134be207f637188e0811a6b81b391665721
Rohit Yadav <yrohit199.new@gmail.com>
2024-03-10 22:08:12 +0530

    initial project setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res)=>{
-    res.send("<h1>your server is alive</h1>")
+    res.send("<h1>your server is alive!</h1>")
 });
 
 app.listen(port, ()=>{
