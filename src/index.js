8f9c75645ecad6c8d5ea49c0d3fe8a8aed0e04e6
Rohit Yadav <yrohit199.new@gmail.com>
2024-03-10 21:50:24 +0530

    this is sample backend project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("<h1>your server is alive</h1>")
+});
+
+app.listen(port, ()=>{
+    console.log(`listening at port ${port}`)
+});
